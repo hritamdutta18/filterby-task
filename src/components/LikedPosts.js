395afa05dd513296d93198a9cc689a03e4d04966
Hritam Dutta <hritamloyola10@gmail.com>
2021-08-20 12:29:43 +0530

    Liked Posts Filter Added

---
+++ src/components/LikedPosts.js
@@ -0,0 +1,14 @@
+import Switch from './Switch'
+
+const LikedPosts = () => {
+    return (
+        <div>
+            <Switch 
+                bhead= 'Liked Posts'/>
+            <hr/>           
+            <br/>
+        </div>
+    )
+}
+
+export default LikedPosts;
