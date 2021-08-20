5abc065421fef4009efa3784950a74baea98f758
Hritam Dutta <hritamloyola10@gmail.com>
2021-08-20 12:30:19 +0530

    Header Added

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className= 'header'>
+            <h1>Filter by</h1>          
+            <br/><br/>
+        </div>
+    )
+}
+
+export default Header;
