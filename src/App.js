0027721fe490052eda332c58cbf68949443cd275
Hritam Dutta <hritamloyola10@gmail.com>
2021-08-22 21:26:32 +0530

    URL Edit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   // Fetch All Posts
   const fetchPosts= async () => {
-    const res= await fetch ('https://my-json-server.typicode.com/hritamdutta18/fake_api_task/db');
+    const res= await fetch ('https://my-json-server.typicode.com/hritamdutta18/fake_api_task/posts');
     const data= await res.json();
 
     return data;
